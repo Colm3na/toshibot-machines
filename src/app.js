9a3ad9f192543adb507c9ff3da401d1b155d9357
wimel <angel.contreras.mejias@gmail.com>
2017-11-24 14:03:47 +0100

    imprimir peticiones

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
-// Importamos modulos
+// Importamos módulos.
 const http = require('http')
-
-// Compactamos codigo, por que la funcion solo es llamada por
-// server.on
+// Función que se ejecuta cuando llegue la petición.
 const server = http.createServer()
-server.on('request', (request, response) =>{
-  response.end('Amparoo no toque naaaah')
+server.on('request', (request, response) => {
+  let body = ''
+  // Concatenamos mas datos de la misma petición.
+  request.on('data', data => {
+    body = body + data
+  })
+  // Evento que se ejecuta cuando se terminan de llegar los datos.
+  request.on('end', () => {
+    console.log(body)
+    response.end('Hola colmena')
+  })
 })
+// El puerto donde está escuchando.
 server.listen(8080)
