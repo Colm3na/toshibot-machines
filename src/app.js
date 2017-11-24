b84484137caec09823a092c0067fc09f08eb8091
wimel <angel.contreras.mejias@gmail.com>
2017-11-24 13:48:55 +0100

    compactando codigo

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 // Importamos modulos
 const http = require('http')
 
-// Crea un servidor web, configuramos la funcion requestHandler
-// como callback para las peticiones
+// Compactamos codigo, por que la funcion solo es llamada por
+// server.on
 const server = http.createServer()
-server.on('request', callback)
-server.listen(8080)
-
-// Funcion que recibe los parametros, el objeto con la peticion
-// y el objeto con la respuesta
-
-function requestHandler(request, response){
+server.on('request', (request, response) =>{
   response.end('Amparoo no toque naaaah')
-}
+})
+server.listen(8080)
